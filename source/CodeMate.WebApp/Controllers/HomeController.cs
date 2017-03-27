48f26298e10117526af244f973648bce3bed8cfb
Wojciech Mioduszewski <wojciech.mioduszewski@outlook.com>
2017-03-27 06:40:46 +0100

    Another experimentall change to see CD in action

--- source/CodeMate.WebApp/Controllers/HomeController.cs
+++ source/CodeMate.WebApp/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace CodeMate.WebApp.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page1.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact page1.";
 
             return View();
         }
