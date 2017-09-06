3088ffc2e4d4165513cb92f25cafc6c30e2d3398
Wojciech Mioduszewski <wojciech.mioduszewski@outlook.com>
2017-09-06 16:56:25 +0100

    Added Debug site to list eevents

---
+++ source/CodHap.RemotePairing/Controllers/DebugController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CodHap.RemotePairing.DAL;
+using CodHap.RemotePairing.Models;
+
+namespace CodHap.RemotePairing.Controllers
+{
+    public class DebugController : Controller
+    {
+        private CalendarDbContext db = new CalendarDbContext();
+
+        // GET: Debug
+        public ActionResult Index()
+        {
+            return View(db.CalendarEvents.ToList());
+        }
+
+        // GET: Debug/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CalendarEvent calendarEvent = db.CalendarEvents.Find(id);
+            if (calendarEvent == null)
+            {
+                return HttpNotFound();
+            }
+            return View(calendarEvent);
+        }
+
+        // GET: Debug/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Debug/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,title,start,end,duration,allDay")] CalendarEvent calendarEvent)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CalendarEvents.Add(calendarEvent);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(calendarEvent);
+        }
+
+        // GET: Debug/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CalendarEvent calendarEvent = db.CalendarEvents.Find(id);
+            if (calendarEvent == null)
+            {
+                return HttpNotFound();
+            }
+            return View(calendarEvent);
+        }
+
+        // POST: Debug/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,title,start,end,duration,allDay")] CalendarEvent calendarEvent)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(calendarEvent).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(calendarEvent);
+        }
+
+        // GET: Debug/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CalendarEvent calendarEvent = db.CalendarEvents.Find(id);
+            if (calendarEvent == null)
+            {
+                return HttpNotFound();
+            }
+            return View(calendarEvent);
+        }
+
+        // POST: Debug/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            CalendarEvent calendarEvent = db.CalendarEvents.Find(id);
+            db.CalendarEvents.Remove(calendarEvent);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
