ab3b3c237b25814f193b58d0e095ede8ea858be4
Wojciech Mioduszewski <wojciech.mioduszewski@outlook.com>
2017-11-25 00:01:05 +0000

    Setting automatic migration possible

--- source/CodHap.RemotePairing/Migrations/Configuration.cs
+++ source/CodHap.RemotePairing/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace CodHap.RemotePairing.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(CodHap.RemotePairing.Models.ApplicationDbContext context)
