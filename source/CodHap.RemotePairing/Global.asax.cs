420b9cdf3422b8146dff3ba072e69ceac256bf61
Wojciech Mioduszewski <wojciech.mioduszewski@outlook.com>
2017-11-29 18:13:03 +0000

    another approach to enable DB migrations for the build automatically

--- source/CodHap.RemotePairing/Global.asax.cs
+++ source/CodHap.RemotePairing/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿namespace CodHap.RemotePairing
 {
     using System.Data.Entity;
+    using System.Data.Entity.Migrations;
     using System.Web;
     using System.Web.Mvc;
     using System.Web.Optimization;
     using System.Web.Routing;
+    using Migrations;
     using Models;
 
     public class MvcApplication : HttpApplication
@@ -16,8 +18,14 @@
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            Database.SetInitializer<ApplicationDbContext>(new MigrateDatabaseToLatestVersion<ApplicationDbContext, CodHap.RemotePairing.Migrations.Configuration>());
+            UpdateDatabaseMigrations();
+        }
 
+        private void UpdateDatabaseMigrations()
+        {
+            var config = new Configuration();
+            var migrator = new DbMigrator(config);
+            migrator.Update();
         }
     }
 }
