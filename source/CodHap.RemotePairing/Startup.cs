05de5a5e4084c8b5ad746a9cf51b2ee8e7b1aa18
Wojciech Mioduszewski <wojciech.mioduszewski@outlook.com>
2020-05-19 06:15:27 +0200

    new world order

--- source/CodHap.RemotePairing/Startup.cs
+++ source/CodHap.RemotePairing/Startup.cs
@@ -1,17 +1,36 @@
-﻿using CodHap.RemotePairing;
-using Microsoft.Owin;
-
-[assembly: OwinStartup(typeof(Startup))]
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace CodHap.RemotePairing
 {
-    using Owin;
-
-    public partial class Startup
+    public class Startup
     {
-        public void Configuration(IAppBuilder app)
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
         {
-            ConfigureAuth(app);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
         }
     }
 }
